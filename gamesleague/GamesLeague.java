8c42c890d319f1e559fb776ce397ef3961947b6d
apxt05 <phengashley@gmail.com>
2025-03-06 10:59:36 +0000

    classes

---
+++ gamesleague/GamesLeague.java
@@ -0,0 +1,14 @@
+package gamesleague;
+
+
+//implements GamesLeagueInterface, manages players leagues and scores
+public class GamesLeague implements GamesLeagueInterface{
+
+    private List<League> leagues;
+    private List<Players> players;
+
+    public GamesLeague() {
+        
+    }
+    
+}
