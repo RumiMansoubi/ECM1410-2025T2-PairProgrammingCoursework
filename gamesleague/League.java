6c691b20fc251c7f944a8aa698cccce679ae2753
RumiMansoubi <09rmansoubi@brightoncollege.net>
2025-03-11 14:08:15 +0000

    Completed League Class

--- gamesleague/League.java
+++ gamesleague/League.java
@@ -1,5 +1,40 @@
 package gamesleague;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class League {
-    
+    private String name;
+    private List<Team> teams;
+
+    public League(String name) {
+        this.name = name;
+        this.teams = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Team> getTeams() {
+        return teams;
+    }
+
+    public void addTeam(Team team) {
+        teams.add(team);
+    }
+
+    public void removeTeam(Team team) {
+        teams.remove(team);
+    }
+
+    public void displayTeams() {
+        for (Team team : teams) {
+            System.out.println(team.getName());
+        }
+    }
 }
