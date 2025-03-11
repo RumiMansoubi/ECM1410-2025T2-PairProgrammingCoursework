feecd4233ef55f7de720897e5dc29966838dee93
RumiMansoubi <09rmansoubi@brightoncollege.net>
2025-03-11 14:31:17 +0000

    WordMasterGame class complete

--- gamesleague/WordMasterGame.java
+++ gamesleague/WordMasterGame.java
@@ -1,5 +1,43 @@
 package gamesleague;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class WordMasterGame {
-    
+    private List<Player> players;
+
+    public WordMasterGame() {
+        this.players = new ArrayList<>();
+    }
+
+    public void addPlayer(Player player) {
+        players.add(player);
+    }
+
+    public void removePlayer(Player player) {
+        players.remove(player);
+    }
+
+    public void playRound() {
+        for (Player player : players) {
+            int score = player.play();
+            player.addScore(score);
+        }
+        awardLeaguePoints();
+    }
+
+    private void awardLeaguePoints() {
+        Collections.sort(players, Comparator.comparingInt(Player::getScore));
+        for (Player player : players) {
+            player.addLeaguePoints(player.getScore());
+        }
+    }
+
+    public void displayScores() {
+        for (Player player : players) {
+            System.out.println(player.getName() + ": " + player.getScore());
+        }
+    }
 }
